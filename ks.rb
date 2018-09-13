d2298cc6685e4bb8f5eadc8c5baa62d3badd8021
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-12 21:15:15 -0500

    Function that takes in current item index and current weight capacity of knapsack

--- ks.rb
+++ ks.rb
@@ -6,3 +6,9 @@ value  = [5, 7, 4, 3, 5, 2, 30]
 
 # Weight capacity of knapsack
 capacity = 10
+
+# Function that takes in current item index and 
+# current weight capacity of knapsack
+def knap_sack indx, capacity
+
+end	

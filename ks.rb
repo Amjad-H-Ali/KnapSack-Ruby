951ce7dbccc9c7b6801cf34674dffa69ebd267c0
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-12 21:49:35 -0500

    Changed variable to global

--- ks.rb
+++ ks.rb
@@ -1,8 +1,8 @@
 p "KnapSack!"
 
 # Items with corresponding weights and values
-weight = [3, 9, 2, 1, 7, 3, 11]
-value  = [5, 7, 4, 3, 5, 2, 30]
+@weight = [3, 9, 2, 1, 7, 3, 11]
+@value  = [5, 7, 4, 3, 5, 2, 30]
 
 # Weight capacity of knapsack
 capacity = 10
@@ -12,25 +12,29 @@ capacity = 10
 # Returns value of KnapSack
 def knap_sack indx, capacity
 
-	value = nil
+	result = nil
 
 	# We've gone through all items or reached the weight limit of the knapsack
 	if indx < 0 || capacity == 0
-		value = 0
+		result = 0
 
 	# If weight of current item exceeds ks capacity, move on to nex item
-	elsif weight[indx] > capacity
-		value = knap_sack indx - 1, capacity
+	elsif @weight[indx] > capacity
+		result = knap_sack(indx - 1, capacity)
 
 	# Evaluate value to be greatest value between putting the item in the ks and moving on
 	# to the next item or leaving item and moving on
 	else
-		move_on = knap_sack index - 1, capacity
+		move_on = knap_sack(indx - 1, capacity)
 
-		put_in = value[indx] + knap_sack index - 1, capacity - weight[indx]
+		put_in = @value[indx] + knap_sack(indx - 1, capacity - @weight[indx])
 
-		value = [move_on, put_in].max
+		result = [move_on, put_in].max
+	end	
 		
-	value
+	result
 
 end	
+
+
+p knap_sack 6, capacity
